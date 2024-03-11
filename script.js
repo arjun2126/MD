ffdfad40cd4b660717b59a47e9d5b2020b833e11
Arjun Subramanya <98678018+arjun2126@users.noreply.github.com>
2024-03-11 09:48:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Kocham Cię! 😘";
+  question.innerHTML = "YAY!";
   gif.src =
     "https://www.icegif.com/wp-content/uploads/love-hug-icegif.gif";
 });
