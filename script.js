e57ce14bb775843e5f8c6ace5ee89f9e3eefe672
Arjun Subramanya <98678018+arjun2126@users.noreply.github.com>
2024-03-13 07:57:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+let scaleFactor = 1;
+
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "You're the best(prettiest) girl in the 🌎";
   gif.src = "https://media0.giphy.com/media/wO4cyxpDbtHji/giphy.gif?cid=6c09b9526zygcv5y2nv0qvg33jqlrv1mc4ru4wksc6ckevfi&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g";
+  
   yesBtn.style.transform = "scale(1.2)";
-  yesBtn.style.display = "none";
   noBtn.style.display = "none";
 });
 
-noBtn.addEventListener("mouseover", () => {
+noBtn.addEventListener("click", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
@@ -22,5 +24,7 @@ noBtn.addEventListener("mouseover", () => {
 
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
-  noBtn.style.transform = "scale(0.8)";
+  
+  scaleFactor -= 0.1;
+  noBtn.style.transform = `scale(${scaleFactor})`;
 });
