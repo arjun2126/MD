dcf90aebedb3b61e2df7e4605124a0b129d4d341
Arjun Subramanya <98678018+arjun2126@users.noreply.github.com>
2024-03-13 21:00:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let scaleFactor = 1;
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Melanie, are you the sun? Because you're the only thing that can light up my nights and make my melatonin levels rise. You're the best(prettiest) girl in the 🌎<br>";
   gif.src = "https://media0.giphy.com/media/wO4cyxpDbtHji/giphy.gif?cid=6c09b9526zygcv5y2nv0qvg33jqlrv1mc4ru4wksc6ckevfi&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g";
-  
+  question.innerHTML = "You're the best(prettiest) girl in the 🌎<br>";
   yesBtn.style.transform = "scale(1.2)";
   yesBtn.style.display = "none";
   noBtn.style.display = "none";
